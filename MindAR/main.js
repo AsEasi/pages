563622c7a0fcaeabe1cc9ad9b4da663c4b83159a
Easi-GH <easi.guo@outlook.com>
2023-10-22 14:14:21 +0800

    Update MindAR : Children's crafting.

--- MindAR/main.js
+++ MindAR/main.js
@@ -17,8 +17,8 @@ window.onload = () => {
 }
 
 function Init() {
-    InitMindAR();
-    InitScene();
+    // InitMindAR();
+    // InitScene();
     InitUI();
 };
 
@@ -34,26 +34,37 @@ const _Clock = new THREE.Clock();
 
 //#region UI
 
-var _UIContainer;
+const _UIContainer = document.getElementById("OverlayUI-Container");
+const _ButtonContainer = document.getElementById("SelectionButtons");
 var _FPSUI;
 
 function InitUI() {
-    // Container :
-
-    _UIContainer = document.createElement("div");
-
-    _UIContainer.id = "UI Container";
-
-    document.body.appendChild(_UIContainer);
-
     // FPS :
 
     _FPSUI = document.createElement("div");
 
     _FPSUI.style.color = "white";
     _FPSUI.style.fontSize = "50px";
+    _FPSUI.style.display = "flex";
+    _FPSUI.style.justifyContent = "center";
+    _FPSUI.style.opacity = 0.25;
 
     _UIContainer.appendChild(_FPSUI);
+
+    // Buttons :
+
+    document.getElementById("Button:Chicken").onclick = ChickenSelected;
+    document.getElementById("Button:Cherry").onclick = CherrySelected;
+    document.getElementById("Button:Cactus").onclick = CactusSelected;
+}
+
+function SetSelectionButtonsVisible(_Visible = true) {
+    if (_Visible) {
+        _ButtonContainer.style.visibility = "visible";
+    }
+    else {
+        _ButtonContainer.style.visibility = "hidden";
+    }
 }
 
 function UpdateUI() {
@@ -67,13 +78,18 @@ function UpdateUI() {
 var _Scene, _Camera, _Renderer;
 var _Anchor0;
 
-const _TargetURL = "Assets/targets_card.mind";
+var _Render = false;
+
+const _MindARContainer = document.getElementById("MindAR-Container");
+var _TargetURL = "Assets/Target_House.mind";
 
 const _FBXLoader = new FBXLoader();
 
+var _TargetModelName = "None";
+
 async function InitMindAR() {
     var _MindAR = new MindARThree({
-        container: document.getElementById("MindAR-Container"),
+        container: _MindARContainer,
         imageTargetSrc: _TargetURL
     });
 
@@ -100,25 +116,110 @@ function InitScene() {
     // _Anchor0.group.add(_Box);
 
     // FBX :
-    _FBXLoader.load(
-        "Assets/Cactus.fbx",
-        (_Model) =>
-        {
-            console.log(_Model);
-            _Model.position.set(0, 0, 0);
-            _Model.scale.set(0.01, 0.01, 0.01);
-            _Anchor0.group.add(_Model);
-        });
+
+    // Clear array.
+    // _Anchor0.group.children.length = 0;
+
+    // Load model.
+    switch (_TargetModelName) {
+        case "Chicken":
+            {
+                _FBXLoader.load(
+                    "Assets/Chicken.fbx",
+                    (_ModelObject) =>
+                    {
+                        console.log(_ModelObject);
+                        _ModelObject.position.set(0, 0, 0);
+                        _ModelObject.scale.set(0.003, 0.003, 0.003);
+                        _Anchor0.group.add(_ModelObject);
+                    });
+                break;
+            }
+        case "Cherry":
+            {
+                _FBXLoader.load(
+                    "Assets/Cherry.fbx",
+                    (_ModelObject) =>
+                    {
+                        console.log(_ModelObject);
+                        _ModelObject.position.set(0, 0, 0);
+                        _ModelObject.scale.set(0.002, 0.002, 0.002);
+                        _ModelObject.rotation.set(0, - Math.PI / 2, 0);
+                        _Anchor0.group.add(_ModelObject);
+                    });
+                break;
+            }
+        case "Cactus":
+            {
+                _FBXLoader.load(
+                    "Assets/Cactus.fbx",
+                    (_ModelObject) =>
+                    {
+                        console.log(_ModelObject);
+                        _ModelObject.position.set(0, 0, 0);
+                        _ModelObject.scale.set(0.01, 0.01, 0.01);
+                        _Anchor0.group.add(_ModelObject);
+                    });
+                break;
+            }
+        default:
+            {
+                console.error("Invalid _TargetModelName : " + _TargetModelName);
+                break;
+            }
+    }
 
     // Lights :
-    const _Light = new THREE.AmbientLight("white", 0.5);
-    _Anchor0.group.add(_Light);
+
+    const _AmbientLight = new THREE.AmbientLight("white", 0.5);
+
+    _Scene.add(_AmbientLight);
+
+    const _DirectionalLight = new THREE.DirectionalLight("white", 5);
+
+    _DirectionalLight.position.set(0, 1, 1);
+
+    _Scene.add(_DirectionalLight);
+    
+    const _DirLightHelper = new THREE.DirectionalLightHelper(_DirectionalLight, 5);
+
+    _Scene.add(_DirLightHelper);
 
     // Button : To DAYOU website.
+
+    // Start rendering
+    _Render = true;
+}
+
+function ChickenSelected() {
+    console.log("Chicken");
+    _TargetModelName = "Chicken";
+    SetSelectionButtonsVisible(false);
+    InitMindAR();
+    InitScene();
+}
+
+function CherrySelected() {
+    console.log("Cherry");
+    _TargetModelName = "Cherry";
+    SetSelectionButtonsVisible(false);
+    InitMindAR();
+    InitScene();
+}
+
+function CactusSelected() {
+    console.log("Cactus");
+    _TargetModelName = "Cactus";
+    SetSelectionButtonsVisible(false);
+    InitMindAR();
+    InitScene();
 }
 
 function Render() {
-    _Renderer.render(_Scene, _Camera);
+    if (_Render) {
+        _Renderer.render(_Scene, _Camera);
+    }
 }
 
 //#endregion
+
